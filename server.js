fa50b334483a279f8dfc075e9c1472d9260e1d4e
Jackie-Hung <757423677@qq.com>
2020-09-21 14:47:51 +0800

    create repository

---
+++ server.js
@@ -0,0 +1,636 @@
+/**************
+ SYSTEM INCLUDES
+**************/
+var	http = require('http');
+var sys = require('sys');
+var	async = require('async');
+var sanitizer = require('sanitizer');
+var compression = require('compression');
+var express = require('express');
+var conf = require('./config.js').server;
+var ga = require('./config.js').googleanalytics;
+
+var scrum = require('./config.js').scrum; // added by steven
+
+/**************
+ LOCAL INCLUDES
+**************/
+var	rooms	= require('./lib/rooms.js');
+var	data	= require('./lib/data.js').db;
+
+/**************
+ GLOBALS
+**************/
+//Map of sids to user_names
+var sids_to_user_names = [];
+
+/**************
+ SETUP EXPRESS
+**************/
+var app = express();
+var router = express.Router();
+
+app.use(compression());
+app.use(conf.baseurl, router);
+
+app.locals.ga = ga.enabled;
+app.locals.gaAccount = ga.account;
+
+router.use(express.static(__dirname + '/client'));
+
+var server = require('http').Server(app);
+server.listen(conf.port);
+
+console.log('Server running at http://127.0.0.1:' + conf.port + '/');
+
+/**************
+ SETUP Socket.IO
+**************/
+var io = require('socket.io')(server, {
+	path: conf.baseurl == '/' ? '' : conf.baseurl + "/socket.io"
+});
+
+
+/**************
+ ROUTES
+**************/
+router.get('/', function(req, res) {
+	//console.log(req.header('host'));
+	url = req.header('host') + req.baseUrl;
+
+	var connected = io.sockets.connected;
+	clientsCount = Object.keys(connected).length;
+
+	res.render('home.jade', {
+		url: url,
+		connected: clientsCount
+	});
+});
+
+
+router.get('/demo', function(req, res) {
+	res.render('index.jade', {
+		pageTitle: 'scrumblr - demo',
+		demo: true
+	});
+});
+
+router.get('/:id', function(req, res){
+	res.render('index.jade', {
+		pageTitle: ('scrumblr - ' + req.params.id)
+	});
+});
+
+
+/**************
+ SOCKET.I0
+**************/
+io.sockets.on('connection', function (client) {
+	//santizes text
+	function scrub( text ) {
+		if (typeof text != "undefined" && text !== null)
+		{
+
+			//clip the string if it is too long
+			if (text.length > 65535)
+			{
+				text = text.substr(0,65535);
+			}
+
+			return sanitizer.sanitize(text);
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+
+
+	client.on('message', function( message ){
+		//console.log(message.action + " -- " + sys.inspect(message.data) );
+
+		var clean_data = {};
+		var clean_message = {};
+		var message_out = {};
+
+		if (!message.action)	return;
+
+		switch (message.action)
+		{
+			case 'initializeMe':
+				initClient(client);
+				break;
+
+			// >>> added by steven
+			case 'sptBurndownChart':
+				getRoom( client, function(room) {
+					db.getAllCards( room , function (cards) {
+						var cardHours = new Map()
+						// var cardHistory = {}
+						cards.forEach( card => {
+							cardHours.set(card.id, {total:card.totalhours})
+						});
+						db.cardHistory(room,function (cardHistory){
+							var burndownData = {};
+
+							// get total hours
+							var hoursTotal = 0;
+							var hoursDay = 0;
+							cardHours.forEach((value, key) => {
+								hoursTotal += parseInt(value['total'])
+							});
+
+							// calculate idea burndown
+							var hoursDay = Math.round(hoursTotal / (scrum.days-1))
+							var idealBurn = []
+							var actualBurn = []
+							var category = []
+							for (let index = 0; index < scrum.days; index++) {
+								remainHours = hoursTotal - index* hoursDay
+								if(remainHours<0)
+									break;
+								
+								category[index] = 'Day' + (index)
+
+								idealBurn.push(remainHours)
+								if(index == (scrum.days - 1) &&  idealBurn[index] != 0 )
+									idealBurn[index] = 0
+							}
+
+							//calculate actual burndown
+							let index = 0
+							actualBurn[index++] = hoursTotal
+							
+							Object.keys(cardHistory).sort().forEach(function(key) {
+								cardHistoryObj = JSON.parse(cardHistory[key])
+								let hours = 0
+								Object.entries(cardHistoryObj).forEach(
+									([k, v]) => {
+										if(cardHours.get(k)){
+											hours += parseInt(v)
+										}
+									}
+								);
+								if (index>0&&hours==0) {
+									index++
+								}else{
+									actualBurn[index++] = hoursTotal - hours
+								}
+								
+								// index++;
+							});
+
+
+							burndownData['idealBurn'] = idealBurn
+							burndownData['categories'] = category
+							burndownData['actualBurn'] = actualBurn
+							client.json.send(
+							{
+								action: 'sptBurndownChart',
+								data: burndownData
+							}
+							);
+						})
+
+						
+			
+					});
+					// deleteCard ( room, clean_message.data.id );
+				});
+				break;
+			// <<<
+
+			case 'joinRoom':
+				joinRoom(client, message.data, function(clients) {
+						client.json.send( { action: 'roomAccept', data: '' } );
+				});
+
+				break;
+
+			case 'moveCard':
+				//report to all other browsers
+				message_out = {
+					action: message.action,
+					data: {
+						id: scrub(message.data.id),
+						position: {
+							left: scrub(message.data.position.left),
+							top: scrub(message.data.position.top)
+						}
+					}
+				};
+
+
+				broadcastToRoom( client, message_out );
+
+				// console.log("-----" + message.data.id);
+				// console.log(JSON.stringify(message.data));
+
+				getRoom(client, function(room) {
+					db.cardSetXY( room , message.data.id, message.data.position.left, message.data.position.top);
+				});
+
+				break;
+
+			case 'createCard':
+			// >>> added by Steven
+				// testCardTimeInsert();
+			// <<< 
+				data = message.data;
+				clean_data = {};
+				clean_data.text = scrub(data.text);
+				clean_data.id = scrub(data.id);
+				clean_data.x = scrub(data.x);
+				clean_data.y = scrub(data.y);
+				clean_data.rot = scrub(data.rot);
+				clean_data.colour = scrub(data.colour);
+
+				clean_data.hours = scrub(data.hours); //receive now hours from client(when you create it is 0)
+				clean_data.totalhours = scrub(data.totalhours); //receive totalhours from client
+				
+				getRoom(client, function(room) {
+					createCard( room, clean_data.id, clean_data.text, clean_data.x, clean_data.y, clean_data.rot, clean_data.colour,clean_data.hours,clean_data.totalhours);
+				});
+
+				message_out = {
+					action: 'createCard',
+					data: clean_data
+				};
+
+				//report to all other browsers
+				broadcastToRoom( client, message_out );
+				break;
+
+			case 'editCard':
+
+				clean_data = {};
+				clean_data.value = scrub(message.data.value);
+				clean_data.id = scrub(message.data.id);
+				clean_data.target = scrub(message.data.target);
+				clean_data.hours = scrub(message.data.hours);
+				clean_data.totalhours = scrub(message.data.totalhours);
+
+				//send update to database
+				getRoom(client, function(room) {
+					db.cardEdit( room , clean_data.id, clean_data.value , clean_data.target );
+				});
+
+				message_out = {
+					action: 'editCard',
+					data: clean_data,
+					target:clean_data.target
+				};
+
+				broadcastToRoom(client, message_out);
+
+				break;
+
+
+			case 'deleteCard':
+				clean_message = {
+					action: 'deleteCard',
+					data: { id: scrub(message.data.id) }
+				};
+
+				getRoom( client, function(room) {
+					db.deleteCard ( room, clean_message.data.id );
+				});
+
+				//report to all other browsers
+				broadcastToRoom( client, clean_message );
+
+				break;
+
+			case 'createColumn':
+				clean_message = { data: scrub(message.data) };
+
+				getRoom( client, function(room) {
+					db.createColumn( room, clean_message.data, function() {} );
+				});
+
+				broadcastToRoom( client, clean_message );
+
+				break;
+
+			case 'deleteColumn':
+				getRoom( client, function(room) {
+					db.deleteColumn(room);
+				});
+				broadcastToRoom( client, { action: 'deleteColumn' } );
+
+				break;
+
+			case 'updateColumns':
+				var columns = message.data;
+
+				if (!(columns instanceof Array))
+					break;
+
+				var clean_columns = [];
+
+				for (var i in columns)
+				{
+					clean_columns[i] = scrub( columns[i] );
+				}
+				getRoom( client, function(room) {
+					db.setColumns( room, clean_columns );
+				});
+
+				broadcastToRoom( client, { action: 'updateColumns', data: clean_columns } );
+
+				break;
+
+			case 'changeTheme':
+				clean_message = {};
+				clean_message.data = scrub(message.data);
+
+				getRoom( client, function(room) {
+					db.setTheme( room, clean_message.data );
+				});
+
+				clean_message.action = 'changeTheme';
+
+				broadcastToRoom( client, clean_message );
+				break;
+
+			case 'setUserName':
+				clean_message = {};
+
+				clean_message.data = scrub(message.data);
+
+				setUserName(client, clean_message.data);
+
+				var msg = {};
+				msg.action = 'nameChangeAnnounce';
+				msg.data = { sid: client.id, user_name: clean_message.data };
+				broadcastToRoom( client, msg );
+				break;
+
+			case 'addSticker':
+				var cardId = scrub(message.data.cardId);
+				var stickerId = scrub(message.data.stickerId);
+
+				getRoom(client, function(room) {
+					db.addSticker( room , cardId, stickerId );
+				});
+
+				broadcastToRoom( client, { action: 'addSticker', data: { cardId: cardId, stickerId: stickerId }});
+				break;
+
+			case 'setBoardSize':
+
+				var size = {};
+				size.width = scrub(message.data.width);
+				size.height = scrub(message.data.height);
+
+				getRoom(client, function(room) {
+					db.setBoardSize( room, size );
+				});
+
+				broadcastToRoom( client, { action: 'setBoardSize', data: size } );
+				break;
+
+			default:
+				//console.log('unknown action');
+				break;
+		}
+	});
+
+	client.on('disconnect', function() {
+			leaveRoom(client);
+	});
+
+  //tell all others that someone has connected
+  //client.broadcast('someone has connected');
+});
+
+
+
+
+
+
+/**************
+ FUNCTIONS
+**************/
+
+// >>> added by steven
+function testCardTimeInsert() {
+    card = {
+        "id":"card95500055",
+        "colour":"blue",
+        "rot": '-3.340326417016475',
+        "x":'316.5327377319336',
+        "y":'123.85032653808594',
+        "text": "" ,
+        "sticker":null,
+        "animationspeed":null,
+        "hours":"15",
+        "totalhours":"20"
+    }
+    db.createCard('demo', 'card95500055', card)
+    db.getAllCards( 'demo' , function (cards) {
+        cards.forEach(card => {
+            if(card['id'] == 'card95500055' && card['hours'] == '15' )
+                return true
+            else  
+                return false
+        });
+    });
+}
+
+
+// <<<
+
+
+
+function initClient ( client )
+{
+	//console.log ('initClient Started');
+	getRoom(client, function(room) {
+
+		db.getAllCards( room , function (cards) {
+
+			client.json.send(
+				{
+					action: 'initCards',
+					data: cards
+				}
+			);
+
+		});
+
+
+		db.getAllColumns ( room, function (columns) {
+			client.json.send(
+				{
+					action: 'initColumns',
+					data: columns
+				}
+			);
+		});
+
+
+		db.getTheme( room, function(theme) {
+
+			if (theme === null) theme = 'bigcards';
+
+			client.json.send(
+				{
+					action: 'changeTheme',
+					data: theme
+				}
+			);
+		});
+
+		db.getBoardSize( room, function(size) {
+
+			if (size !== null) {
+				client.json.send(
+					{
+						action: 'setBoardSize',
+						data: size
+					}
+				);
+			}
+		});
+
+		roommates_clients = rooms.room_clients(room);
+		roommates = [];
+
+		var j = 0;
+		for (var i in roommates_clients)
+		{
+			if (roommates_clients[i].id != client.id)
+			{
+				roommates[j] = {
+					sid: roommates_clients[i].id,
+					user_name:  sids_to_user_names[roommates_clients[i].id]
+					};
+				j++;
+			}
+		}
+
+		//console.log('initialusers: ' + roommates);
+		client.json.send(
+			{
+				action: 'initialUsers',
+				data: roommates
+			}
+		);
+
+	});
+}
+
+
+function joinRoom (client, room, successFunction)
+{
+	var msg = {};
+	msg.action = 'join-announce';
+	msg.data		= { sid: client.id, user_name: client.user_name };
+
+	rooms.add_to_room_and_announce(client, room, msg);
+	successFunction();
+}
+
+function leaveRoom (client)
+{
+	//console.log (client.id + ' just left');
+	var msg = {};
+	msg.action = 'leave-announce';
+	msg.data	= { sid: client.id };
+	rooms.remove_from_all_rooms_and_announce(client, msg);
+
+	delete sids_to_user_names[client.id];
+}
+
+function broadcastToRoom ( client, message ) {
+	rooms.broadcast_to_roommates(client, message);
+}
+
+//----------------CARD FUNCTIONS
+function createCard( room, id, text, x, y, rot, colour,hours,totalhours ) {
+	//var hoursprfix = 100;
+	var card = {
+		id: id,
+		colour: colour,
+		rot: rot,
+		x: x,
+		y: y,
+		text: text,
+		sticker: null,
+		animationspeed: null,
+		hours: hours,
+		totalhours: totalhours,
+	};
+
+	db.createCard(room, id, card);
+}
+
+
+function roundRand( max )
+{
+	return Math.floor(Math.random() * max);
+}
+
+
+
+//------------ROOM STUFF
+// Get Room name for the given Session ID
+function getRoom( client , callback )
+{
+	room = rooms.get_room( client );
+	//console.log( 'client: ' + client.id + " is in " + room);
+	callback(room);
+}
+
+// >>> added by steven
+function sptBurndownChart( client , callback )
+{
+	room = rooms.get_room( client );
+	//console.log( 'client: ' + client.id + " is in " + room);
+	callback(room);
+}
+
+// <<<
+
+
+function setUserName ( client, name )
+{
+	client.user_name = name;
+	sids_to_user_names[client.id] = name;
+	//console.log('sids to user names: ');
+	console.dir(sids_to_user_names);
+}
+
+function cleanAndInitializeDemoRoom()
+{
+	// DUMMY DATA
+	db.clearRoom('/demo', function() {
+		db.createColumn( '/demo', 'Not Started' );
+		db.createColumn( '/demo', 'Started' );
+		db.createColumn( '/demo', 'Testing' );
+		db.createColumn( '/demo', 'Review' );
+		db.createColumn( '/demo', 'Complete' );
+
+
+		createCard('/demo', 'card1', 'Hello this is fun', roundRand(600), roundRand(300), Math.random() * 10 - 5, 'yellow');
+		createCard('/demo', 'card2', 'Hello this is a new story.', roundRand(600), roundRand(300), Math.random() * 10 - 5, 'white');
+		createCard('/demo', 'card3', '.', roundRand(600), roundRand(300), Math.random() * 10 - 5, 'blue');
+		createCard('/demo', 'card4', '.', roundRand(600), roundRand(300), Math.random() * 10 - 5, 'green');
+
+		createCard('/demo', 'card5', 'Hello this is fun', roundRand(600), roundRand(300), Math.random() * 10 - 5, 'yellow');
+		createCard('/demo', 'card6', 'Hello this is a new card.', roundRand(600), roundRand(300), Math.random() * 10 - 5, 'yellow');
+		createCard('/demo', 'card7', '.', roundRand(600), roundRand(300), Math.random() * 10 - 5, 'blue');
+		createCard('/demo', 'card8', '.', roundRand(600), roundRand(300), Math.random() * 10 - 5, 'green');
+	});
+}
+//
+
+/**************
+ SETUP DATABASE ON FIRST RUN
+**************/
+// (runs only once on startup)
+var db = new data(function() {
+	cleanAndInitializeDemoRoom();
+});
